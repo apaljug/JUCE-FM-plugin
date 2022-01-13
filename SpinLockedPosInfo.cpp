d9e1bc002428ee63997037baff1a0d47b756fa54
rayxke <keary.d.mobley@gmail.com>
2022-01-13 18:50:02 +0800

    First refactor of code

---
+++ SpinLockedPosInfo.cpp
@@ -0,0 +1,28 @@
+//
+//  SpinLockedPosInfo.cpp
+//  AudioPluginDemo - All
+//
+//  Created by Keary Mobley on 1/13/22.
+//  Copyright © 2022 JUCE. All rights reserved.
+//
+
+#include "SpinLockedPosInfo.h"
+
+SpinLockedPosInfo::SpinLockedPosInfo() { info.resetToDefault(); }
+
+    // Wait-free, but setting new info may fail if the main thread is currently
+    // calling `get`. This is unlikely to matter in practice because
+    // we'll be calling `set` much more frequently than `get`.
+void SpinLockedPosInfo::set (const AudioPlayHead::CurrentPositionInfo& newInfo)
+{
+    const juce::SpinLock::ScopedTryLockType lock (mutex);
+
+    if (lock.isLocked())
+        this->info = newInfo;
+}
+
+AudioPlayHead::CurrentPositionInfo SpinLockedPosInfo::get() const noexcept
+{
+    const juce::SpinLock::ScopedLockType lock (mutex);
+    return this->info;
+}
