a731893686eddb9bd41792fd61e757339730ccc5
rayxke <keary.d.mobley@gmail.com>
2022-01-19 10:43:09 +0800

    Update Plugin Code

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -56,7 +56,7 @@
  #define JucePlugin_ManufacturerCode       0x4d616e75
 #endif
 #ifndef  JucePlugin_PluginCode
- #define JucePlugin_PluginCode             0x50627a6a
+ #define JucePlugin_PluginCode             0x50627a6b
 #endif
 #ifndef  JucePlugin_IsSynth
  #define JucePlugin_IsSynth                1
